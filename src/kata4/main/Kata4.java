e14650b48660581c25cc648ae7ad4468708afa99
usuario <usuario@10.22.148.65>
2017-11-03 11:41:56 +0000

    Kata4;version2

--- src/kata4/main/Kata4.java
+++ src/kata4/main/Kata4.java
@@ -10,11 +10,25 @@ import kata4.view.MailHistogramBuilder;
 import kata4.view.MailListReader;
 
 public class Kata4 {
-
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    private static List<Mail>  mailList;
+    private static Histogram<String> histogram;
+    
+    public static void main(String[] args) throws FileNotFoundException, IOException {        
+        execute();
+    }
+    private static void execute() throws FileNotFoundException, IOException{
+        input();
+        process();
+        output();
+    }
+    private static void input() throws FileNotFoundException, IOException{
         String filename = "C:\\Users\\usuario\\Desktop\\emails.txt";
-        List<Mail>  mailList = MailListReader.read(filename);
-        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
+        mailList = MailListReader.read(filename);
+    }
+    private static void process(){
+        histogram = MailHistogramBuilder.build(mailList);
+    }
+    private static void output(){
         HistogramDisplay histoDisplay = null;
         new HistogramDisplay(histogram).execute(); 
     }
